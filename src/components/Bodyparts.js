1cc8844a61f9de9d7e6f01734ecc951337222929
ShadyJoseph <shadyjosephabadeer2003@gmail.com>
2024-03-30 19:02:57 +0200

    gymB

--- src/components/Bodyparts.js
+++ src/components/Bodyparts.js
@@ -1,6 +1,6 @@
 // Bodyparts.js
 import React from 'react';
-import Icon from '../assets/assets/icons/gym.png';
+import Icon from '../assets/assets/icons/gymB.png';
 import './style.css';
 
 const Bodyparts = ({ item, setBodyPart, bodyPart }) => {
@@ -9,15 +9,15 @@ const Bodyparts = ({ item, setBodyPart, bodyPart }) => {
   };
 
   return (
-    <div className="text-center bodypart-container">
-      <p className="mb-2">{item}</p>
+    <div className="text-center bodypart-container ">
+      <p className="mb-2 text-primary text-capitalize fs-5">{item}</p>
       <button
         className="btn mt-2 bodypart-button"
         style={{
-          width: '200px',
-          height: '200px',
+          width: '180px',
+          height: '180px',
           borderRadius: '10px',
-          boxShadow: item === bodyPart ? ' 5px 5px 17px 0px rgba(36,149,255,0.27)': 'none',
+          borderTop: item === bodyPart ? '4px solid blue' : 'none'
         }}
         onClick={handleClick}
       >
