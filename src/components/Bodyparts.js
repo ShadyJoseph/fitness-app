f4f38b3a2bbaa3de14589417593d969a3ca3f326
ShadyJoseph <shadyjosephabadeer2003@gmail.com>
2024-03-30 18:22:49 +0200

    itemName

--- src/components/Bodyparts.js
+++ src/components/Bodyparts.js
@@ -1,6 +1,7 @@
 // Bodyparts.js
 import React from 'react';
 import Icon from '../assets/assets/icons/gym.png';
+import './style.css';
 
 const Bodyparts = ({ item, setBodyPart, bodyPart }) => {
   const handleClick = () => {
@@ -8,22 +9,19 @@ const Bodyparts = ({ item, setBodyPart, bodyPart }) => {
   };
 
   return (
-    <div className="text-center">
-      <p className="mb-2">{item.name}</p>
+    <div className="text-center bodypart-container">
+      <p className="mb-2">{item}</p>
       <button
-        className="btn btn-outline-primary mt-2"
+        className="btn mt-2 bodypart-button"
         style={{
-          width: '150px',
-          height: '150px',
-          gap:'45px',
-          cursor:'pointer',
+          width: '200px',
+          height: '200px',
           borderRadius: '10px',
-          boxShadow: item === bodyPart ? '0px 0px 10px #0d6efd' : 'none',
-          transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+          boxShadow: item === bodyPart ? ' 5px 5px 17px 0px rgba(36,149,255,0.27)': 'none',
         }}
         onClick={handleClick}
       >
-        <img src={Icon} alt="Gym Icon" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        <img src={Icon} alt="Gym Icon" style={{ width: "50px", height: "50px" }} />
       </button>
     </div>
   );
--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,5 +1,7 @@
+// HorizontalScrollBar.js
 import React, { useState } from 'react';
 import Bodyparts from './Bodyparts';
+import './style.css';
 
 const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
   const [startIndex, setStartIndex] = useState(0);
@@ -15,15 +17,15 @@ const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="d-flex flex-nowrap justify-content-between align-items-center" >
+    <div className="container mt-5 scrollbar-container">
+      <div className="d-flex flex-nowrap justify-content-between align-items-center" style={{ overflowX: 'hidden' }}>
         {data.slice(startIndex, startIndex + 3).map((item, index) => (
           <div key={item.id || index} className="mr-3">
             <Bodyparts item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
           </div>
         ))}
       </div>
-      <button className="btn btn-primary mt-3" onClick={handleClickPrev} disabled={startIndex === 0}  >
+      <button className="btn btn-primary mt-3" onClick={handleClickPrev} disabled={startIndex === 0}>
         Previous
       </button>
       <button className="btn btn-primary mt-3 ms-2" onClick={handleClickNext} disabled={startIndex >= data.length - 3}>
